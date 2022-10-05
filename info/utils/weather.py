f052c2f52a68472cc76a5bc757865e4bc2bb39c7
Nirav Shah <nshah28@ncsu.edu>
2022-10-04 23:56:40 -0400

    updated weather.py

--- info/utils/weather.py
+++ info/utils/weather.py
@@ -14,3 +14,17 @@ class WeatherUtilBase(ABC):
     @abstractmethod
     def get_city_weather(self, city: str, **kwargs):
         pass
+
+
+class WeatherBit(WeatherUtilBase):
+    def get_city_weather(self, city: str, **kwargs):
+        params = self._url.with_default_params({"city": city})
+        
+
+        response = requests.request(
+            "GET", str(self._url.get_url(path="/current")),
+            
+            params=params,
+        )
+
+        return response.json()
