b99b6e263118a811a5514d10d90a304989be1a7c
Vishwa Gandhi <111928135+vishwagandhi1610@users.noreply.github.com>
2022-10-04 23:05:48 -0400

    added the foursquare class in places.py

--- info/utils/places.py
+++ info/utils/places.py
@@ -14,3 +14,16 @@ class PlacesUtilBase(ABC):
     @abstractmethod
     def get_places(self, city: str, **kwargs):
         pass
+    
+class FourSquare(PlacesUtilBase):
+    def get_places(self, city: str, **kwargs):
+        params = self._url.with_default_params({"near": city})
+        params.update(kwargs)
+
+        response = requests.request(
+            "GET", str(self._url.get_url(path="/v3/places/search")),
+            headers=self._url.with_default_headers(),
+            params=params
+        )
+
+        return response.json()
