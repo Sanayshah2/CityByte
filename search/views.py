a404ec4dbb3fbeaf42ad048028d06e2ea908bdbf
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2022-10-05 18:42:06 -0400

    Added city_info HTML file

--- search/views.py
+++ search/views.py
@@ -2,25 +2,29 @@ from django.http import JsonResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 
+from info.helpers.weather import WeatherBitHelper
 from search.helpers.autocomplete import GenericDBSearchAutoCompleteHelper
 from search.helpers.photo import UnplashCityPhotoHelper
 from info.helpers.places import FourSquarePlacesHelper
 from search.utils.search import AmadeusCitySearch
 from search.utils.url import URL
 
+
 @require_http_methods(["GET"])
 def main_page(request):
     return render(request, 'search/search.html')
+
 
 @require_http_methods(["GET"])
 def city_suggestions(request):
     suggestions_data = GenericDBSearchAutoCompleteHelper(
         klass=AmadeusCitySearch, url=URL(**settings.AMADEUS_CONFIG)
     ).get_suggestions(city=request.GET.get("q"), max=10)
-    
+
     return JsonResponse({
         "results": suggestions_data.get("data", [])
     })
+
 
 @require_http_methods(["GET"])
 def city_photo(request):
@@ -28,10 +32,13 @@ def city_photo(request):
     return JsonResponse({
         "path": photo_link
     })
+
+
 @require_http_methods(["GET"])
 def place_photo(request):
     photo_link = FourSquarePlacesHelper().get_place_photo(fsq_id=request.GET.get('fsq_id'))
     return redirect(photo_link)
+
 
 @require_http_methods(["GET"])
 def info_page(request):
