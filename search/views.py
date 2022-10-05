77a9a170d1d0e1fa4f752a2c552cdb7bae410a5c
Priya Saroj <67330167+Priya-Saroj@users.noreply.github.com>
2022-10-05 12:02:59 -0400

    added view for search html file

--- search/views.py
+++ search/views.py
@@ -1,8 +1,12 @@
 from django.http import JsonResponse
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_http_methods
 
 from search.helpers.autocomplete import GenericDBSearchAutoCompleteHelper
 
+@require_http_methods(["GET"])
+def main_page(request):
+    return render(request, 'search/search.html')
 
 @require_http_methods(["GET"])
 def city_suggestions(request):
