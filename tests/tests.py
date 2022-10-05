dd3bce6cec0ffcfcd44b72e17d43495f848b9453
Nirav Shah <nshah28@ncsu.edu>
2022-10-05 17:21:17 -0400

    Updated test case

--- tests/tests.py
+++ tests/tests.py
@@ -7,6 +7,6 @@ def test_photo():
     photo_link = FourSquarePlacesHelper().get_place_photo(fsq_id='518a71ab498e430858000827')
     #assert photo_link == 'https://fastly.4sqi.net/img/general/250x250/33256190_KwhKlZ90963X-l1Vzda668M0CFO1Lxujlg2TIsySBfM.jpg'
     site = urlopen(photo_link)
-    meta = site.info()  # get header of the http request
-    if meta["content-type"] in image_formats:  # check if the content-type is a image
+    meta = site.info()  
+    if meta["content-type"] in image_formats: 
         assert True
