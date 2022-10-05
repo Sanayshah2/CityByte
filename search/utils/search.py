7b135915411da991dd8d69f6795f82cab97c9278
Vishwa Gandhi <111928135+vishwagandhi1610@users.noreply.github.com>
2022-10-05 01:06:37 -0400

    updated the search in utils of search

--- search/utils/search.py
+++ search/utils/search.py
@@ -1,6 +1,19 @@
-import requests
+from abc import ABC, abstractmethod
 
-from search.utils.baseutils.search import SearchUtilBase
+import requests
+from django.conf import settings
+
+from search.utils.url import URL
+
+class SearchUtilBase(ABC):
+    _url: URL = None
+
+    def __init__(self, url: URL):
+        self._url = url
+
+    @abstractmethod
+    def get_city_suggestions(self, city: str, **kwargs):
+        pass
 
 
 class GeoDB(SearchUtilBase):
