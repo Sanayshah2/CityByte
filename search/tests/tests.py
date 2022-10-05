595fda52a5778e857c3666c67f0a8c634aac823f
VrushankiPatel <111774275+VrushankiPatel@users.noreply.github.com>
2022-10-05 15:15:06 -0400

    Update tests.py

--- search/tests/tests.py
+++ search/tests/tests.py
@@ -5,8 +5,6 @@ image_formats = ("image/png", "image/jpeg", "image/gif")
 
 def test_photo():
     photo_link = FourSquarePlacesHelper().get_place_photo(fsq_id='518a71ab498e430858000827')
-    # print("**************",request.GET.get('fsq_id'))
-    # print("...........",photo_link)
     #assert photo_link == 'https://fastly.4sqi.net/img/general/250x250/33256190_KwhKlZ90963X-l1Vzda668M0CFO1Lxujlg2TIsySBfM.jpg'
     site = urlopen(photo_link)
     meta = site.info()  # get header of the http request
