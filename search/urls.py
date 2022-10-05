7e06109de950d32c3be1e50fceab434ae29f2b3b
Pradyumna Khawas <pradyumna.khawas@gmail.com>
2022-10-05 19:45:26 -0400

    Added city photos path to urls

--- search/urls.py
+++ search/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import city_suggestions
+from .views import city_suggestions, city_photo
 
 urlpatterns = [
     path('city', city_suggestions),
+    path('city/photo', city_photo),
 ]
