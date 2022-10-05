225e3b0b7334b5f43363d768959b2ed75bd0cb73
VrushankiPatel <111774275+VrushankiPatel@users.noreply.github.com>
2022-10-05 00:40:27 -0400

    Update python.py

--- search/helpers/python.py
+++ search/helpers/python.py
@@ -5,3 +5,9 @@ from django.conf import settings
 from search.utils.photo import PhotoUtilBase
 from search.utils.photo import Unsplash
 from search.utils.url import URL
+
+
+class CityPhotoHelperBase(ABC):
+    @abstractmethod
+    def get_city_photo(self, city: str):
+        pass
