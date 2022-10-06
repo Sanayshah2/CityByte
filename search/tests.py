eddb2e8fe870d0d3c632edd164b45463636c40fc
Niravsshaha2 <111940049+Niravsshaha2@users.noreply.github.com>
2022-10-06 18:35:32 -0400

    Update tests.py

--- search/tests.py
+++ search/tests.py
@@ -17,7 +17,9 @@ image_formats = ("image/png", "image/jpeg", "image/gif")
 class CityByte_testcase(TestCase):
   def test_main_page(self):
       assert (render(request, 'search/search.html').status_code ==200)
-  def test_cityphoto():
+      
+      
+  def test_cityphoto(self):
     photo_link = UnplashCityPhotoHelper().get_city_photo(city='Pune')
     site = urlopen(photo_link)
     meta = site.info() 
