e005c7ad5b5325dfa9af58f2b237ab98aeba49fc
Nirav Shah <nshah28@ncsu.edu>
2022-10-06 06:46:51 -0400

    Added test case for city photo

---
+++ search/tests/test_cityphoto.py
@@ -0,0 +1,21 @@
+from info.helpers.places import FourSquarePlacesHelper
+from search.helpers.photo import UnplashCityPhotoHelper
+from django.test import TestCase
+from urllib.request import urlopen
+image_formats = ("image/png", "image/jpeg", "image/gif")
+
+
+# def test_photo():
+#     photo_link = FourSquarePlacesHelper().get_place_photo(fsq_id='518a71ab498e430858000827')
+#     site = urlopen(photo_link)
+#     meta = site.info()  
+#     if meta["content-type"] in image_formats: 
+#         assert True
+        
+        
+def test_cityphoto():
+    photo_link = UnplashCityPhotoHelper().get_city_photo(city='Pune')
+    site = urlopen(photo_link)
+    meta = site.info() 
+    if meta["content-type"] in image_formats: 
+        assert True
--- search/tests/test_photo.py
+++ search/tests/test_photo.py
@@ -5,17 +5,17 @@ from urllib.request import urlopen
 image_formats = ("image/png", "image/jpeg", "image/gif")
 
 
-# def test_photo():
-#     photo_link = FourSquarePlacesHelper().get_place_photo(fsq_id='518a71ab498e430858000827')
-#     site = urlopen(photo_link)
-#     meta = site.info()  
-#     if meta["content-type"] in image_formats: 
-#         assert True
-        
-        
-def test_cityphoto():
-    photo_link = UnplashCityPhotoHelper().get_city_photo(city='Pune')
+def test_photo():
+    photo_link = FourSquarePlacesHelper().get_place_photo(fsq_id='518a71ab498e430858000827')
     site = urlopen(photo_link)
-    meta = site.info() 
+    meta = site.info()  
     if meta["content-type"] in image_formats: 
         assert True
+        
+        
+# def test_cityphoto():
+#     photo_link = UnplashCityPhotoHelper().get_city_photo(city='Pune')
+#     site = urlopen(photo_link)
+#     meta = site.info() 
+#     if meta["content-type"] in image_formats: 
+#         assert True
