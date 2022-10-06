a1e13ef99c3dfa884868ecb6a3e03d0571f2b0e4
Nirav Shah <nshah28@ncsu.edu>
2022-10-06 06:48:18 -0400

    updated test oinfo

--- search/tests/test_info.py
+++ search/tests/test_info.py
@@ -1,4 +1,17 @@
-
+from django.test import TestCase
+from info.helpers.places import FourSquarePlacesHelper
+from search import views
+import pytest
+from search.helpers.autocomplete import GenericDBSearchAutoCompleteHelper
+from django.shortcuts import render, redirect
+from info.helpers.weather import WeatherBitHelper
+from datetime import datetime
+import pytz
+from pytz import timezone
+from search.helpers.photo import UnplashCityPhotoHelper
+from search.utils.search import AmadeusCitySearch
+from urllib.request import urlopen
+image_formats = ("image/png", "image/jpeg", "image/gif")
 
 def test_info_page(request):
     city = 'New York City'
