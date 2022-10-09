c7cb6c6f9a198a078f5c0d8e2934a4af8875da50
Niravsshaha2 <111940049+Niravsshaha2@users.noreply.github.com>
2022-10-09 19:24:53 -0400

    Update tests.py

--- search/tests.py
+++ search/tests.py
@@ -1,8 +1,6 @@
 from urllib import request
 from django.test import TestCase
 from info.helpers.places import FourSquarePlacesHelper
-from search import views
-import pytest
 from search.helpers.autocomplete import GenericDBSearchAutoCompleteHelper
 from django.shortcuts import render, redirect
 from info.helpers.weather import WeatherBitHelper
@@ -10,7 +8,6 @@ from datetime import datetime
 import pytz
 from pytz import timezone
 from search.helpers.photo import UnplashCityPhotoHelper
-from search.utils.search import AmadeusCitySearch
 from urllib.request import urlopen
 image_formats = ("image/png", "image/jpeg", "image/gif")
 
